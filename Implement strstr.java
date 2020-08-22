46c5b5c88a09f8a0afa1a167bcac691bac926361
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-08-22 19:46:29 +0530

    Add files via upload

---
+++ Implement strstr.java
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+import java.util.*;
+
+
+class Implement_strstr
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String a = line.split(" ")[0];
+			String b = line.split(" ")[1];
+			
+			GfG g = new GfG();
+			System.out.println(g.strstr(a,b));
+			
+			t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*
+the function returns the 
+position where the target string 
+matches the string str
+
+Your are required to complete this method */
+
+class GfG
+{
+    int strstr(String str, String target)
+    {
+       return str.indexOf(target);
+    }
+}
