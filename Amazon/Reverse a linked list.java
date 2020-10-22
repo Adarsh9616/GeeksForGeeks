ba203704f0956fbad361bb6c7dc914d56ca78b7d
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-23 04:28:28 +0530

    Add files via upload

---
+++ Amazon/Reverse a linked list.java
@@ -0,0 +1,38 @@
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class ReverseLL
+{
+    // This function should reverse linked list and return
+    // head of the modified linked list.
+    Node reverseList(Node head)
+    {
+        if(head==null)
+        {
+            return null;
+        }
+        Node curr=head;
+        Node prev=null;
+        Node next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
