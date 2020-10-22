19df934060678449ea2625476d72c625d0fde87c
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-23 04:28:52 +0530

    Add files via upload

---
+++ Amazon/Left View of Binary Tree.java
@@ -0,0 +1,50 @@
+
+//User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Tree
+{
+    ArrayList<Integer> leftView(Node root)
+    {
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        if(root==null)
+        {
+            return ans;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                
+                Node temp=q.poll();
+                if(i==0)
+                {
+                    ans.add(temp.data);
+                }
+                if(temp.left!=null)
+                {
+                    q.add(temp.left);
+                }
+                if(temp.right!=null)
+                {
+                    q.add(temp.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
